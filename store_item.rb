0121875270a25c2dd6903a6fd95a82d85cfe0087
phil <phil.pondo@gmail.com>
2020-05-20 13:44:31 -0400

    Adds store item inventory

---
+++ store_item.rb
@@ -0,0 +1,22 @@
+# Three hashes describing items in my store's inventory
+
+item1 = {
+  :item => "grill",
+  :color => "silver",
+  :price => 99.99,
+  :brand => "Grillmaster"
+}
+
+item2 = {
+  :item => "twinkies",
+  :color => "yellow",
+  :price => 2.99,
+  :brand => "Hostess"
+}
+
+item3 = {
+  item: "socks, 4-pack",
+  color: "white",
+  price: 9.99,
+  brand: "Hane's"
+}
